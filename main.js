f73039036e0c974c498f75e5338c35b7cf1ee8e1
LeNelSoN <98358578+LeNelSoN@users.noreply.github.com>
2022-05-02 16:45:29 +0200

    Add files via upload
    My First Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let Section1 = document.getElementById('part-1');
+let Section2 = document.getElementById('part-2');
+let Section3 = document.getElementById('part-3');
+
+const Sections = [Section1,Section2,Section3];
+
+let button1 = document.getElementById('btn-1');
+let button2 = document.getElementById('btn-2');
+let button3 = document.getElementById('btn-3');
+
+//const Buttons = [button1,button2,button3]
+
+//for (let i = 0; i < Buttons.length; i++) {
+//    Buttons[i].addEventListener("click", () => {
+//        for (let index = 1; index < Sections[i].children.length; index++) {
+        //    let childrens = Sections[i].children[index]
+      //      if (childrens.classList.contains('hide')) {
+    //            childrens.classList.remove('hide');
+  //              childrens.classList.add('show');
+//            } else {
+          //     childrens.classList.add('hide');
+        //       childrens.classList.remove('show');
+      //      };
+    //    };
+  //  });
+//};
